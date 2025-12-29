14dc9a64acebc571bba86c5f12b5180259d509f4
Ziqi Wang <qiqiustc@gmail.com>
2025-12-29 11:28:55 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -579,8 +579,8 @@ if __name__ == '__main__':
     default_settings['ipm'] = {
         'float_type': torch.float64,
         "ipm_iter": 30,
-        "pcg_iter_1": 200,
-        "pcg_iter_2": 200,
+        "pcg_iter_1": 400,
+        "pcg_iter_2": 100,
     }
     contacts = compute_assembly_contacts(parts, default_settings)
     init_rbe(parts, contacts, default_settings)
