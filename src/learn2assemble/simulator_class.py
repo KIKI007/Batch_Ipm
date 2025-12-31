c660dcf4a90592ca367ef4fb17a3601520f1379a
Ziqi Wang <qiqiustc@gmail.com>
2025-12-31 16:07:59 +0800

    Update simulator_class.py

--- src/learn2assemble/simulator_class.py
+++ src/learn2assemble/simulator_class.py
@@ -559,7 +559,7 @@ class IpmSim(torch.nn.Module):
 
     def forward(self, x):
         new_settings = {}
-        print(self.iAs)
+        print(self.get_buffer)
         for n, val in self.ipm_settings.items():
             if torch.is_tensor(val):
                 new_settings[n] = self.get_buffer(n)
