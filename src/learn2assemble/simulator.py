66e0a6481b6783793b92b890b8ea54b81883287d
Ziqi Wang <qiqiustc@gmail.com>
2025-12-31 13:38:18 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -631,6 +631,8 @@ class IpmSim(torch.nn.Module):
             if torch.is_tensor(val):
                 self.ipm_settings[n] = self.get_buffer(n)
                 self.ipm_settings['device'] = self.ipm_settings[n].device
+                print(self.ipm_settings[n].device)
+        print(x.device)
         return simulate_ipm(x, self.ipm_settings)
 
 def simulate(parts: list[Trimesh],
@@ -702,8 +704,10 @@ if __name__ == '__main__':
     end_timer('auto parameter')
 
     sim = IpmSim(default_settings["ipm"])
-    model = torch.nn.DataParallel(sim)
+    sim = torch.nn.DataParallel(sim)
+
     print("start simulation")
+    part_states = part_states.to(device = 'cuda:1')
     v_fp32, stable_fp32 = sim(part_states)
     print_logger(stable_fp32.shape[0])
     #print_logger(part_states.shape[0])
