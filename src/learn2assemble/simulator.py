748eef2476ac34f86df87a3ab9cf07b3f2a98833
Ziqi Wang <qiqiustc@gmail.com>
2026-01-01 14:47:54 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -78,7 +78,7 @@ def G_(p, nλn, nt, nf, mu):
     return torch.vstack([-λn, -p, p])
 
 
-@torch.compile(disable=disable_compile)
+@torch.compile(disable=disable_compile, dynamic=False)
 def GTZSG_(p, ZS, nλn, nt, nf, mu):
     nbatch = p.shape[1]
     nλt = nλn * nt
@@ -104,7 +104,7 @@ def GTZSG_(p, ZS, nλn, nt, nf, mu):
     return λ3 - λ2 + λ0
 
 
-@torch.compile(disable=disable_compile)
+@torch.compile(disable=disable_compile, dynamic=False)
 def Q_(p, nλn, nt, iA, iB, nA, nB, invM, Q):
     batch = p.shape[1]
     nλt = nλn * nt
