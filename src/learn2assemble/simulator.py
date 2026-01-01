6e6a21a865f03e19a8dc94acd5591cebbabd9aca
Ziqi Wang <qiqiustc@gmail.com>
2026-01-01 16:33:52 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -715,7 +715,7 @@ if __name__ == '__main__':
     part_states = part_states[inds, :]
 
     # random
-    part_states = part_states[:n_batch, :].repeat((8, 1))
+    part_states = part_states[:n_batch, :].repeat((1, 1))
     # default_settings['gurobi'] = {}
     default_settings['ipm'] = {
         "n_iter": 25,
