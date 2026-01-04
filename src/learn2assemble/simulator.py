bb86f1d6c5256d6a6de7d06938f6d23a478a77e8
Ziqi Wang <qiqiustc@gmail.com>
2026-01-04 14:07:46 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -708,7 +708,7 @@ if __name__ == '__main__':
     default_settings['rbe']['mu'] = 0.2
     default_settings["assembly"]["contact_shrink_ratio"] = 0.1  # for robustnessly computing the contact surfaces
 
-    n_batch = 1024
+    n_batch = 2048
     torch.manual_seed(0)
     name = "tetris-999"
     parts = load_assembly_from_files(ASSEMBLY_RESOURCE_DIR + f"/{name}")
