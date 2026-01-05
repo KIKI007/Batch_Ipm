79fc7ec7db4c4b13c587e341f4a9c7a1fbf3a538
Ziqi Wang <qiqiustc@gmail.com>
2026-01-05 21:25:20 +0800

    Update batch_simulation.py

--- script/batch_simulation.py
+++ script/batch_simulation.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     sol_files = [f for f in listdir(curriculumn_folder) if isfile(join(curriculumn_folder, f))]
     sol_files.sort()
     sol_files = sol_files[::-1]
-    sol_files = sol_files[:64]
+    sol_files = sol_files[:10]
 
     dict_ipm_settings = parallel_load_assembly(sol_files, n_worker=32)
     dict_part_states = load_states(sol_files)
