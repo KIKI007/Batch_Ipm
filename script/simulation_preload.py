62041dcef80bcea06170350af77de58f82060305
Ziqi Wang <qiqiustc@gmail.com>
2026-01-05 19:39:17 +0800

    Update simulation_preload.py

--- script/simulation_preload.py
+++ script/simulation_preload.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
         for sol_file in sol_files:
             ipm_settings_cpu = load_assembly(sol_file)
             dict_ipm_settings[sol_file] = ipm_settings_cpu
-            memory = sys.getsizeof(dict_ipm_settings[sol_file])/1024/1024
+            memory = sys.getsizeof(dict_ipm_settings[sol_file])/1024.0/1024.0
             progress.set_postfix_str(f"{memory:.2f}")
             progress.update()
 
