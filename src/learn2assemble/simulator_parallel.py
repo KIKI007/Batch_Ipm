5b51dc69dd52a8af980c5442833f4fa4ddeb1132
Ziqi Wang <qiqiustc@gmail.com>
2026-01-09 11:59:04 +0800

    Update simulator_parallel.py

--- src/learn2assemble/simulator_parallel.py
+++ src/learn2assemble/simulator_parallel.py
@@ -285,7 +285,7 @@ if __name__ == '__main__':
     name = "tetris-999"
     parts = load_assembly_from_files(ASSEMBLY_RESOURCE_DIR + f"/{name}")
     boundary = [0]
-    default_settings['gurobi'] = {"nsim": 16}
+    default_settings['gurobi'] = {"nsim": 64}
     default_settings['env']['boundary_part_ids'] = boundary
 
     filename = os.path.join(RESOURCE_DIR, f"curriculum/{name}.pt")
