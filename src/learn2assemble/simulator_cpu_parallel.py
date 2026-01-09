1c3d8c5a946c2aee61aad35c691c5f73875f4535
Ziqi Wang <qiqiustc@gmail.com>
2026-01-09 16:37:26 +0800

    update

--- src/learn2assemble/simulator_cpu_parallel.py
+++ src/learn2assemble/simulator_cpu_parallel.py
@@ -56,8 +56,8 @@ if __name__ == '__main__':
 
     default_settings['rbe']['mu'] = 0.2
     default_settings["assembly"]["contact_shrink_ratio"] = 0.1  # for robustnessly computing the contact surfaces
-
-    n_batch = 1024
+    default_settings['gurobi'] = {}
+    n_batch = 2048
     torch.manual_seed(0)
     name = "tetris-999"
     parts = load_assembly_from_files(ASSEMBLY_RESOURCE_DIR + f"/{name}")
